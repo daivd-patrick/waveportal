674202e984315c5fafde7850c9ce544ea399f7aa
tiffany-ymt <tiffanyymt70@gmail.com>
2021-10-17 09:09:53 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const wave = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="max-w-md mx-auto p-10">
+      <div className="flex flex-col items-center space-y-8">
+        <h1 className="text-4xl font-bold text-gray-900 text-center">
+          👋 Hey there!
+        </h1>
+        <p className="text-gray-600 max-w-md">
+          I am Andres and I am learning web3 and smart contracts!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          className="px-4 py-2 rounded-xl bg-blue-600 text-white"
+          onClick={wave}
         >
-          Learn React
-        </a>
-      </header>
+          Wave at me!
+        </button>
+      </div>
     </div>
   );
 }
