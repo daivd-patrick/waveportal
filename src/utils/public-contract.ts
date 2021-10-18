07b28994c111aac53ffe5b1a311fb69673c54fc2
tiffany-ymt <tiffanyymt70@gmail.com>
2021-10-18 14:13:41 -0500

    add etherscan api key

--- src/utils/public-contract.ts
+++ src/utils/public-contract.ts
@@ -1,7 +1,10 @@
 import { ethers } from 'ethers';
 import { contractAddress, contractABI } from './constants';
 
-const provider = new ethers.providers.EtherscanProvider('rinkeby');
+const provider = new ethers.providers.EtherscanProvider(
+  'rinkeby',
+  process.env.REACT_APP_ETHERSCAN_API_KEY
+);
 
 /** Uses the public Etherscan provider on the Rinkeby testnet */
 export const publicContract = new ethers.Contract(
