e7db8608788ed0228857233ed95e2a75359cb558
tiffany-ymt <tiffanyymt70@gmail.com>
2021-10-17 22:16:33 -0500

    hook up the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useMachine } from '@xstate/react';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { wavePortalMachine } from './wave-portal.machine';
+
 function App() {
-  const wave = () => {};
+  const [current, send] = useMachine(wavePortalMachine, { devTools: true });
+  const queryClient = useQueryClient();
+  const waveDataQuery = useQuery<number>(
+    'waveData',
+    async () => {
+      const count = await current.context.contract!.getTotalWaves();
+      return count.toNumber();
+    },
+    { enabled: Boolean(current.context.contract) }
+  );
+  const waveMutation = useMutation(async () => {
+    const waveTxn = await current.context.contract!.wave();
+    await waveTxn.wait();
+    const data = queryClient.getQueryData<number>('waveData');
+    queryClient.setQueryData('waveData', data! + 1);
+    queryClient.invalidateQueries('waveData');
+  });
 
   return (
     <div className="max-w-md mx-auto p-10">
@@ -10,13 +30,43 @@ function App() {
         <p className="text-gray-600 max-w-md">
           I am Andres and I am learning web3 and smart contracts!
         </p>
-        <button
-          className="px-4 py-2 rounded-xl bg-blue-600 text-white"
-          onClick={wave}
-        >
-          Wave at me!
-        </button>
+        <div className="space-x-2 ml-auto">
+          {['waitingForWallet', 'connectingWallet'].some(current.matches) && (
+            <button
+              className="px-4 py-2 rounded-xl bg-gray-200"
+              onClick={() => send('CONNECT_WALLET')}
+            >
+              {current.matches('connectingWallet')
+                ? 'Continue in Metamask 🦊'
+                : 'Connect Wallet'}
+            </button>
+          )}
+          {current.matches('checkingIfWalletIsConnected') && (
+            <div>Loading wallet...</div>
+          )}
+          {current.matches('idle') && (
+            <button
+              className="px-4 py-2 rounded-xl bg-blue-600 text-white"
+              onClick={() => waveMutation.mutate()}
+              disabled={waveMutation.isLoading}
+            >
+              {waveMutation.isLoading ? 'Mining...' : 'Wave at me!'}
+            </button>
+          )}
+        </div>
       </div>
+      {current.matches('idle') && (
+        <>
+          <div>Account: {current.context.account}</div>
+          <div>
+            {waveDataQuery.isLoading
+              ? 'Loading wave data...'
+              : waveDataQuery.isError
+              ? 'There was an error fetching wave data :('
+              : `Total waves: ${waveDataQuery.data}`}
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { inspect } from '@xstate/inspect';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+inspect({
+  iframe: false,
+});
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
