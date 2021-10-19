45d25ba35ccea411077d3a139f517a584a41cd15
tiffany-ymt <tiffanyymt70@gmail.com>
2021-10-19 06:32:27 -0500

    shorten address on mobile, don't show year unless it's different

--- src/components/wave-list-item.tsx
+++ src/components/wave-list-item.tsx
@@ -3,25 +3,29 @@ import { AccountAvatar } from './account-avatar';
 
 export function WaveListItem({ wave }: { wave: Wave }) {
   return (
-    <li className="space-x-2 flex">
+    <li className="flex space-x-2">
       <AccountAvatar
-        className="w-10 h-10 flex-shrink-0 inline-block"
+        className="flex-shrink-0 inline-block w-10 h-10"
         account={wave.address}
       />
-      <div className="inline-block relative rounded-2xl px-6 py-4 bg-gray-200 w-full">
+      <div className="relative inline-block w-full px-6 py-4 bg-gray-200 rounded-2xl">
         <a
           href={`https://rinkeby.etherscan.io/address/${wave.address}`}
           target="_blank"
           rel="noreferrer"
         >
-          <small className="font-mono text-gray-600 hover:text-blue-500 hover:underline font-medium">
+          <small className="hidden font-mono font-medium text-gray-600 md:inline hover:text-blue-500 hover:underline">
             {wave.address}
+          </small>
+          <small className="font-mono font-medium text-gray-600 hover:text-blue-500 hover:underline md:hidden">
+            {wave.address.substr(0, 4)}...
+            {wave.address.substr(wave.address.length - 4)}
           </small>
         </a>
         <div className="my-2">{wave.message}</div>
         <time
           dateTime={wave.timestamp.toISOString()}
-          className="absolute bottom-3 right-3 text-xs text-gray-600"
+          className="absolute text-xs text-gray-600 bottom-3 right-3"
         >
           {
             [
@@ -40,7 +44,7 @@ export function WaveListItem({ wave }: { wave: Wave }) {
             ][wave.timestamp.getMonth()]
           }{' '}
           {wave.timestamp.getDay()}
-          {2020 !== new Date().getFullYear() &&
+          {wave.timestamp.getFullYear() !== new Date().getFullYear() &&
             `, ${wave.timestamp.getFullYear()}`}
         </time>
       </div>
